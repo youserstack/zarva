3e0e05065af67c606458f8c57b624e98f5ed7253
youserstack <youserstack@gmail.com>
2024-11-27 16:02:48 +0900

    update

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -8,16 +8,25 @@ export default function CategoryButton() {
 
   return (
     <div className="relative hidden md:block text-white">
+      <div
+        className={`fixed inset-0 bg-black z-[100] 
+        transition-opacity duration-[0.7s] ease-in-out
+        ${open ? "opacity-50" : "opacity-0 pointer-events-none"}`}
+        onClick={() => setOpen(false)}
+      />
+
       <button
-        className={`rounded-md px-2 py-[1px] outline-none hover:bg-neutral-200/20
-          ${open ? "bg-neutral-200/20 " : ""} `}
+        className={`relative rounded-md px-2 py-[1px] outline-none hover:bg-neutral-200/20 z-[100]
+        ${open ? "bg-neutral-200/20 " : ""} `}
         onClick={() => setOpen(!open)}
-        onBlur={() => setOpen(false)}
       >
         카테고리
       </button>
 
-      <div className={`${open ? "block" : "hidden"} absolute top-full z-[100] min-w-[100px]`}>
+      <div
+        className={`absolute top-full min-w-[100px] z-[100] 
+        ${open ? "block" : "hidden"}  `}
+      >
         <CategoryMenu />
       </div>
     </div>
--- src/components/skeletons/SearchBoxSkeleton.tsx
+++
@@ -1,5 +0,0 @@
-export default function SearchBoxSkeleton() {
-  return (
-    <div className="animate-pulse w-[300px] h-[26px] rounded-md bg-neutral-100 dark:bg-neutral-700"></div>
-  );
-}
